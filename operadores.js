58362c04ecd09418383c056cefc7fb10aceff28b
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-11-22 10:55:18 -0300

    operadores e relacionais

---
+++ operadores.js
@@ -0,0 +1,63 @@
+/* operarações aritméticas
+
+var soma = 1 + 4
+console.log(soma)
+
+var subtracao = 10 - 3
+console.log(subtracao)
+
+var divisao = 50 / 5
+console.log(divisao)
+
+var multiplicacao = 3 * 3
+console.log(multiplicacao)
+
+var porcentagem = 18 % 4
+console.log(porcentagem)
+*/
+
+
+/* incrementar e decrementar 
+
+var soma = 21 + 1
+console.log(soma++)
+console.log(soma)
+console.log(soma--)
+console.log(soma)
+*/
+
+/* operadores aritméticos abreviados
+
+var soma = 20
+soma += 21
+console.log(soma)
+soma -= 17
+console.log(soma)
+*/
+
+/* operadores de igualdade
+var soma = 1
+console.log(1 == 1)
+//true
+console.log(soma =! 1)
+//false
+var nome = 'Fernando'
+var outroNome = 'Maria'
+console.log(nome === outroNome)
+//false
+var num = 2
+var outroNum = '2'
+console.log(num == outroNum)
+// true, pois o JavaScript tenta converter para o mesmo valor 
+console.log(num === outroNum)
+// false, pois foi verificado tanto o mesmo valor quanto o mesmo tipo
+*/
+
+// relacionais
+var num = 100
+console.log(num > 1000)
+//false
+console.log(num >= 100)
+//true
+console.log(num < 200)
+//true
