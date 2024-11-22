550317a7300ff002648a4e5fe98ce7559b958e94
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-11-22 10:11:17 -0300

    objetos e acessar propriedades com array

---
+++ exercicios/script.js
@@ -0,0 +1,11 @@
+pessoa = {
+    altura: 1.62,
+    peso: 69
+}
+console.log(pessoa)
+pessoa.altura = 1.80
+console.log(pessoa.altura)
+pessoa.peso = 80
+console.log(pessoa)
+console.log(pessoa["altura"])
+// tipos de dados em JavaScript com os objetos e arrays
