f7125fea8809a036d1b7cf7b41135e93072f881b
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-11-22 11:58:53 -0300

    funções

---
+++ function.js
@@ -0,0 +1,34 @@
+/*var x = 1;
+
+function soma () {
+    x = x + 1
+}
+console.log(soma())
+console.log(x)
+*/
+
+function qualquer () {
+    var fernando = 'Fernando'
+   console.log(`Olá, ${fernando}!`)
+}
+qualquer()
+/* 1 - Cria uma função
+   2 - Cria uma variável dentro da função
+   3- Exibe no console concatenando como desejar ( ainda dentro da função)
+   4 - Não precisa de return, porém para exibir o resultado precisa chamar a função fora dela
+*/
+
+/*function saudacao () {
+    var nome = 'Maria'
+    return `Olá, ${nome}, seja bem-vinda!`
+}
+
+var mensagem = saudacao()
+console.log(mensagem)
+/* 1- Cria uma função
+   2- Cria uma variável dentro da função
+   3- Insere o return para retornar o objeto 
+   4- Cria uma outra variável chamando a função como seu valor atribuído
+   5- Exibe no console a variável criada obtendo o resultado do "return"
+*/
+
